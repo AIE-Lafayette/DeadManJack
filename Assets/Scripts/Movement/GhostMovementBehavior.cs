1094e27fbd64a22b9b23f9d89b195aaa013d01fa
Joshua <JCoshua360@gmail.com>
2022-05-04 17:00:22 -0500

    Added Ghost Teleport (Unfinished)

--- Assets/Scripts/Movement/GhostMovementBehavior.cs
+++ Assets/Scripts/Movement/GhostMovementBehavior.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GhostMovementBehavior : EnemyMovementBehavior
@@ -9,28 +10,37 @@ public class GhostMovementBehavior : EnemyMovementBehavior
     [SerializeField]
     private float _rightBoundry = 5;
     private Vector3 _moveDirection;
-
+    private float _teleportTimer = 1;
+    private float _timeUntillNextTeleport = 0;
     public override void Update()
     {
-        float distance = transform.position.z - Target.position.z;
-        if (distance > 1)
+        if(_timeUntillNextTeleport > _teleportTimer)
         {
-            if (transform.position.x <= _leftBoundry)
-                _moveDirection = new Vector3(1, 0, -1);
-            else if (transform.position.x >= _rightBoundry)
-                _moveDirection = new Vector3(-1, 0, -1);
-        }
-        else
-        {
-            if (transform.position.x <= _leftBoundry)
-                _moveDirection = new Vector3(1, 0, 0);
-            else if (transform.position.x >= _rightBoundry)
-                _moveDirection = new Vector3(-1, 0, 0);
-            else if (transform.position.x >= _leftBoundry && transform.position.x <= _rightBoundry)
-                _moveDirection = new Vector3(_moveDirection.x, 0, 0);
-        }
+            GameObject[] actors = SceneManager.GetActiveScene().GetRootGameObjects();
+            GameObject[] enemies = new GameObject[0];
 
-        Velocity = _moveDirection * Speed;
-        base.Update();
+            for (int i = 0; i < actors.Length; i++)
+            {
+                if (actors[i].CompareTag("Enemy"))
+                {
+                    GameObject[] tempArray = new GameObject[enemies.Length + 1];
+                    for (int j = 0; j < enemies.Length; j++)
+                        tempArray[j] = enemies[j];
+
+                    tempArray[enemies.Length] = actors[i];
+                    enemies = tempArray;
+                }
+            }
+
+            int enemyToProtect = Random.Range(0, enemies.Length);
+
+            if (enemyToProtect != enemies.Length)
+            {
+                transform.position = (enemies[enemyToProtect].transform.position - Target.position) / 2;
+            }
+
+            _timeUntillNextTeleport = 0;
+        }
+        _timeUntillNextTeleport += Time.deltaTime;
     }
 }
