0c25fab01535fdd630e5c119f8d26cb5fd4f1763
Joshua <JCoshua360@gmail.com>
2022-05-09 16:46:14 -0500

    Added Wave variables to the Game Manager

--- Assets/Scripts/Gameplay/GameManagerBehavior.cs
+++ Assets/Scripts/Gameplay/GameManagerBehavior.cs
@@ -6,8 +6,22 @@ public class GameManagerBehavior : MonoBehaviour
 {
     [SerializeField]
     private GameObject _player;
-    private int _waveCount;
-    private int _enemyCount;
+
+    private int _waveCount = 1;
+    private int _enemyCount = 0;
+    private int _waveSize = 0;
+    private bool _waveOver = false;
+
+    [SerializeField]
+    private float _enemySpawnTime = 5.0f;
+    private float _spawnTimer = 0.0f;
+    private float _waveCooldown = 15.0f;
+    private float _waveCooldownTimer = 0.0f;
+
+    [SerializeField]
+    private EnemySpawnerBehavior _zombieSpawner;
+    private EnemySpawnerBehavior _skeltonSpawner;
+    private EnemySpawnerBehavior _ghostSpawner;
 
     public GameObject Player
     {
@@ -18,5 +32,18 @@ public class GameManagerBehavior : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void WaveManager()
+    {
+        if (_waveOver)
+        {
+            if (_waveCooldownTimer == 0)
+                _waveCount++;
+            _waveCooldownTimer += Time.deltaTime;
+        }
+
+
+
     }
 }
