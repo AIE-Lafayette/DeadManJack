3b28fb7864e715cc36f88e6abe5ad5e7d5df4a8a
nicholasMeyers <s218019@student.aie.edu>
2022-05-13 11:06:59 -0500

    Added a Routine Behavior for a Timer

--- Assets/Scripts/Gameplay/Abilities/AbilityBehavior.cs
+++ Assets/Scripts/Gameplay/Abilities/AbilityBehavior.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class AbilityBehavior : MonoBehaviour
 {
     private GameObject _owner;
+    private GameObject _visualPrefab;
 
     public GameObject Owner
     {
         get { return _owner; }
         set { _owner = value; }
+    }
+
+    public GameObject VisualPrefab
+    {
+        get { return _visualPrefab; }
+        set { _visualPrefab = value; }
     }
 
     public virtual void Activate(params object[] arguments)
