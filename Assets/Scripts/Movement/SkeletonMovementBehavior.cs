2cedb64774b2d3199a7b8749d230c3a744e340f5
Joshua <JCoshua360@gmail.com>
2022-05-16 10:36:37 -0500

    Removed Skeleton Movement Mechanic

--- Assets/Scripts/Movement/SkeletonMovementBehavior.cs
+++ Assets/Scripts/Movement/SkeletonMovementBehavior.cs
@@ -4,26 +4,9 @@ using UnityEngine;
 
 public class SkeletonMovementBehavior : EnemyMovementBehavior
 {
-    private float _moveTimer = 0.0f;
-    private bool _wasHit = false;
     public override void Update()
     {
-        if (!_wasHit)
-            Velocity = new Vector3(0, 0, -1);
-        else
-        {
-            if(transform.position.x > 0)
-                Velocity = new Vector3(-1, 0, 0);
-            else
-                Velocity = new Vector3(1, 0, 0);
-            _moveTimer += Time.deltaTime;
-
-            if(_moveTimer > 1)
-            {
-                _wasHit = false;
-                _moveTimer = 0;
-            }
-        }
+        Velocity = new Vector3(0, 0, -1);
 
         base.Update();
     }
@@ -35,11 +18,5 @@ public class SkeletonMovementBehavior : EnemyMovementBehavior
 
         base.OnBeingGrabbed(player);
         player.CurrentAbility = Ability.SKELETON;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        _wasHit = true;
-        _moveTimer = 0.0f;
     }
 }
