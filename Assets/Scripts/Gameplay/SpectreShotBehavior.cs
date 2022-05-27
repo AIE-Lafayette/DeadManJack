f7cc88e3028364d0e5c960ae7479153502beb51c
nicholasMeyers <s218019@student.aie.edu>
2022-05-27 10:30:42 -0500

    Began Working on the Spectral Shot

---
+++ Assets/Scripts/Gameplay/SpectreShotBehavior.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectreShotBehavior : BulletBehavior
+{
+    /// <summary>
+    /// The game object that the shot will leave behind, slowing the enemies within.
+    /// </summary>
+    [SerializeField]
+    private GameObject _spectralResidue;
+
+    /// <summary>
+    /// The mesh renderer for the bullet.
+    /// </summary>
+    [SerializeField]
+    private MeshRenderer _meshRenderer;
+
+    /// <summary>
+    /// The collider for the bullet.
+    /// </summary>
+    [SerializeField]
+    private SphereCollider _collider;
+
+    /// <summary>
+    /// The timed action for figuring out when the bullet should burst.
+    /// </summary>
+    private RoutineBehavior.TimedAction _timedAction;
+
+    /// <summary>
+    /// After the bullet travels in the air for a time, or collides with an enemy, it bursts and sets the spectral residue active. Putting a timer on that.
+    /// </summary>
+    public void Burst()
+    {
+        // Disables the mesh renderer and the collider for the bullet.
+        _meshRenderer.enabled = false;
+        _collider.enabled = false;
+
+        // Sets the residue that remains after the shot active, and puts a timer for it to dissipate.
+        _spectralResidue.SetActive(true);
+        RoutineBehavior.Instance.StartNewTimedAction(arguments => Dissipate(), TimedActionCountType.SCALEDTIME, 4f);
+    }
+
+    /// <summary>
+    /// Once the timer for the spectral residue runs out, it vanishes.
+    /// </summary>
+    public void Dissipate()
+    {
+        Destroy(gameObject);
+    }
+
+    protected void Awake()
+    {
+        base.Awake();
+        _timedAction = RoutineBehavior.Instance.StartNewTimedAction(arguments => Burst(), TimedActionCountType.SCALEDTIME, 3f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EnemyMovementBehavior enemy = other.GetComponent<EnemyMovementBehavior>();
+
+        if (enemy)
+        {
+            Burst();
+            RoutineBehavior.Instance.StopTimedAction(_timedAction);
+        }
+    }
+}
