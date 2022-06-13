b9bad9d3a0f5968b0dd6ce3500970aaeb76fb3fe
Joshua <JCoshua360@gmail.com>
2022-06-13 10:34:02 -0500

    Fixed bugs cause by animations

--- Assets/Scripts/Gameplay/EnemyBehaviors/SkeletonHeadBehavior.cs
+++ Assets/Scripts/Gameplay/EnemyBehaviors/SkeletonHeadBehavior.cs
@@ -6,7 +6,6 @@ public class SkeletonHeadBehavior : EnemyBehavior
 {
     [SerializeField]
     private SkeletonBehavior _body;
-    private bool _isGrounded = false;
     private HealthBehavior _health;
 
     /// <summary>
@@ -32,8 +31,9 @@ public class SkeletonHeadBehavior : EnemyBehavior
     {
         base.Update();
 
-        if (_isGrounded)
+        if (transform.position.y < 0)
         {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
             Movement.Velocity = new Vector3(0, 5f, -1);
         }
         else
@@ -45,22 +45,6 @@ public class SkeletonHeadBehavior : EnemyBehavior
         if(!_health.IsAlive)
         {
             Destroy(_body.gameObject);
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.collider.name == "Plane")
-        {
-            _isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.collider.name == "Plane")
-        {
-            _isGrounded = false;
         }
     }
 }
